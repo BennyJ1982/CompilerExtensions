954e967cae7108b7e2845174e13a95f2434b1ee6
Benjamin Janecke <benjamin_j@gmx.de>
2016-03-03 17:15:56 +0100

    Initial commit

---
+++ MethodUpperCase/MethodUpperCase/Analyzers/MissingModuleConfig.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Refactorings.Analyzers
+{
+	using System.Linq;
+	using Microsoft.CodeAnalysis.CSharp;
+	using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public class MissingModuleConfig : DiagnosticAnalyzer
+	{
+		public const string DiagnosticId = "MissingModuleConfig";
+		internal static readonly LocalizableString Title = "Missing module configuration";
+		internal static readonly LocalizableString MessageFormat = "No module configuration file could be found for '{0}.{1}'.";
+		internal const string Category = "FACTON";
+
+		internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, true);
+
+		private const string ModuleInterface = "IModule";
+		private const string ModuleNamespace = "Facton.Infrastructure.Modularity";
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+		public override void Initialize(AnalysisContext context)
+		{
+			context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.ClassDeclaration);
+		}
+
+		private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+		{
+			INamedTypeSymbol factonModule;
+			if (!TryGetFactonModule(context, out factonModule))
+			{
+				return;
+			}
+
+			// TODO check if config file exists
+
+			context.ReportDiagnostic(Diagnostic.Create(Rule, context.Node.GetLocation(), factonModule.ContainingNamespace, factonModule.Name));
+		}
+
+		private static bool TryGetFactonModule(SyntaxNodeAnalysisContext context, out INamedTypeSymbol factonModule)
+		{
+			var classDeclarationSyntax = (ClassDeclarationSyntax)context.Node;
+			if (classDeclarationSyntax.BaseList == null)
+			{
+				factonModule = null;
+				return false;
+			}
+
+			var baseTypeIdentifiers = classDeclarationSyntax.BaseList.Types.Select(t => t.Type).OfType<IdentifierNameSyntax>();
+			if (!baseTypeIdentifiers.Any(b => b.Identifier.ValueText.Equals(ModuleInterface)))
+			{
+				factonModule = null;
+				return false;
+			}
+
+			// syntactically matching base type found. Check if it also macthes semantically
+			var module = context.SemanticModel.GetDeclaredSymbol(classDeclarationSyntax);
+			foreach (var moduleInterface in module.Interfaces)
+			{
+				if (moduleInterface.ContainingNamespace.ToString() == ModuleNamespace && moduleInterface.Name == ModuleInterface)
+				{
+					// found a FACTON module
+					factonModule = module;
+					return true;
+				}
+			}
+
+			factonModule = null;
+			return false;
+		}
+	}
+}
---
+++ MethodUpperCase/MethodUpperCase/CodeFixes/CreateModuleConfig.cs
@@ -0,0 +1,31 @@
+﻿namespace Refactorings.CodeFixes
+{
+	using System;
+	using System.Collections.Immutable;
+	using System.Composition;
+	using System.Threading.Tasks;
+	using Microsoft.CodeAnalysis;
+	using Microsoft.CodeAnalysis.CodeFixes;
+
+	[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(CreateModuleConfig)), Shared]
+	public class CreateModuleConfig : CodeFixProvider
+	{
+		// TODO: Replace with actual diagnostic id that should trigger this fix.
+		public const string DiagnosticId = "CreateModuleConfig";
+
+		public sealed override ImmutableArray<string> FixableDiagnosticIds
+		{
+			get { return ImmutableArray.Create(DiagnosticId); }
+		}
+
+		public sealed override FixAllProvider GetFixAllProvider()
+		{
+			return WellKnownFixAllProviders.BatchFixer;
+		}
+
+		public sealed override Task RegisterCodeFixesAsync(CodeFixContext context)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
